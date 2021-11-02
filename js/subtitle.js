8dd34657d6b4947610376cd7b7bb57248df1d4dc
Aleksi Tarvainen <a.aleksi.tarvainen@gmail.com>
2021-11-02 17:44:32 +0200

    Some small tweaks to layout and styles

--- js/subtitle.js
+++ js/subtitle.js
@@ -1,27 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
     const st = document.getElementById("subtitle")
-    st.innerHTML = "Hello world"
+    st.innerHTML = "Hello, world!"
     const titles = ["Software Developer", "Information technology student", "Haskeller", "Huge Nerd"]
     const changeTitle = (i) => {
         st.innerHTML = titles[i]
     }
+
     changeTitle(0)
     const titleDisplayTime = 8000
     setTimeout(() => {
         st.classList.remove("fade")
     })
+
     setTimeout(() => {
         st.classList.add("fade")
     }, titleDisplayTime - 1000)
+
     let i = 0
     setInterval(() => {
         st.classList.remove("fade")
         i = (i + 1) % titles.length
         changeTitle(i)
-        console.log("interval", st.classList, titles[i])
         setTimeout(() => {
             st.classList.add("fade")
-            console.log("timeout", st.classList, titles[i])
         }, titleDisplayTime - 1000)
     }, titleDisplayTime)
 })
